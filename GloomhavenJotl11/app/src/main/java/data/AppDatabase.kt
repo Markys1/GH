60463b7819d5c7b91dab9a998bcdf4805022ce96
Markys1 <140750895+Markys1@users.noreply.github.com>
2026-01-28 16:16:43 +0500

    Delete GloomhavenJotl11 directory

--- GloomhavenJotl11/app/src/main/java/data/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-
-@Database(entities = [CharacterEntity::class], version = 1)
-@TypeConverters(Converters::class)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun dao(): CharacterDao
-}
--- GloomhavenJotl11/app/src/main/java/data/CharacterClass.kt
+++
@@ -1,6 +0,0 @@
-enum class CharacterClass {
-    RED_GUARD,
-    VOIDWARDEN,
-    HATCHET,
-    DEMOLITIONIST
-}
